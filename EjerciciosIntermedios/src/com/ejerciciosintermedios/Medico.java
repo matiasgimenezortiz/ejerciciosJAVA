8bb4b402583e9f9700ccc65b7b56183a532df92e
matiasgimenezortiz <matias.gimenezortiz@gmail.com>
2020-05-25 19:23:24 -0300

    Commit de ejercicios intermedios con clase Medico

---
+++ EjerciciosIntermedios/src/com/ejerciciosintermedios/Medico.java
@@ -0,0 +1,19 @@
+package com.ejerciciosintermedios;
+
+/**
+ * Ejericio o clase con la que se definirán atributos y métodos para crear un objeto de tipo médico.
+ * El médico tendrá nombre, apellido, edad, casado, especialista.
+ * Se definirá un constructor, los getters y los setters.
+ */
+
+public class Medico {
+    private String nombre,apellido;
+    private int edad;
+    private boolean casado, especialista;
+
+    public Medico(String nombre, String apellido, int edad, boolean casado, boolean especialista){
+        
+    }
+
+
+}
