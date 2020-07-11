c427f7d0ebfc6859642b9b28f45be747beee002f
Matías <matias.gimenezortiz@gmail.com>
2020-07-11 15:28:41 -0300

    Commit with all the OOP exercises of HackerRank

---
+++ HackerRankExercices/src/com/oop/JOverriding.java
@@ -0,0 +1,46 @@
+package com.oop;
+/*
+Note: When overriding a method, you should precede it with the @Override annotation. The parameter(s) and return type
+of an overridden method must be exactly the same as those of the method inherited from the supertype.
+
+Task
+Complete the code in your editor by writing an overridden getNumberOfTeamMembers method that prints the same statement
+as the superclass' getNumberOfTeamMembers method, except that it replaces  with  (the number of players on a Soccer team).
+ */
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+
+
+}
+
+public class JOverriding {
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+}
